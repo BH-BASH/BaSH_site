9c233a26949c0f6bc0077ae4366aca2dddd0274c
遠山金時@遠山研究室室長 <71078288+BH-BASH@users.noreply.github.com>
2022-06-11 04:28:43 +0900

    Create server.js

---
+++ server.js
@@ -0,0 +1,41 @@
+'use strict';
+
+// モジュール
+const http     = require('http');
+const express  = require('express');
+const socketIO = require('socket.io');
+const moment   = require('moment');
+
+// オブジェクト
+const app    = express();
+const server = http.Server(app);
+const io     = socketIO(server);
+
+// 定数
+const PORT = process.env.PORT || 3000;
+
+// グローバル変数
+let iCountUser = 0; // ユーザー数
+
+// 接続時の処理
+io.on('connection', (socket) => {
+    console.log('connection');
+
+    // 切断時の処理
+    socket.on('disconnect', () => {
+        console.log('disconnect');
+    });
+
+    // 新しいメッセージ受信時の処理
+    socket.on('new message', (strMessage) => {
+        console.log('new message', strMessage);
+    });
+});
+
+// 公開フォルダの指定
+app.use(express.static(__dirname + '/public'));
+
+// サーバーの起動
+server.listen(PORT, () => {
+    console.log('server starts on port: %d', PORT);
+});
