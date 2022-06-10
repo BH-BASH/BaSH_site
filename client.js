b7f85b99afaee3d5bf829db70970601080f0b525
遠山金時@遠山研究室室長 <71078288+BH-BASH@users.noreply.github.com>
2022-06-11 04:27:04 +0900

    Create client.js

---
+++ client.js
@@ -0,0 +1,26 @@
+'use strict';
+
+// クライアントからサーバーへの接続要求
+const socket = io.connect();
+
+// 接続時の処理
+socket.on('connect', () => {
+    console.log('connect');
+});
+
+// 「Send」ボタンを押したときの処理
+$('form').submit(() => {
+    const $inp = $('#input_message');
+    const text = $inp.val();
+
+    console.log('#input_message :', text);
+
+    if (text) {
+        // サーバーに、イベント名 'new message' で入力テキストを送信
+        socket.emit('new message', text);
+        // テキストボックスを空に
+        $inp.val('');
+    }
+    // フォーム送信はしない
+    return false;
+});
